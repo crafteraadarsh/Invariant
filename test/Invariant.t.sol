e1bf8eda40efc90c49874cdc2268fb980e7058ea
Aadarsh Singh <95958161+crafteraadarsh@users.noreply.github.com>
2024-03-11 11:11:01 +0530

    Update Invariant.t.sol

---
+++ test/Invariant.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.2 <0.9.0;
+
+import "forge-std/Test.sol";
+import "contracts/Deposit.sol";
+contract InvariantDeposit is Test {
+    Deposit deposit;
+
+    function setUp() external {
+        deposit = new Deposit();
+        vm.deal(address(deposit), 100 ether);
+    }
+
+     function invariant_alwaysWithdrawable() external payable {
+        vm.startPrank(address(0xaa));
+        vm.deal(address(0xaa), 10 ether);
+
+        deposit.deposit{value: 1 ether}();
+        uint256 balanceBefore = deposit.balance(address(0xaa));
+        vm.stopPrank();
+        assertEq(balanceBefore, 1 ether);
+
+        vm.prank(address(0xaa));
+        deposit.withdraw();
+        uint256 balanceAfter = deposit.balance(address(0xaa));
+        vm.stopPrank();
+        assertGt(balanceBefore, balanceAfter);
+    }
+
+    receive() external payable {}
+}
